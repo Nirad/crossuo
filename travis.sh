449f317cfc2c2bb150e72f4875449956a897d3b6
Nirad <miljours@gmail.com>
2019-02-24 22:04:27 -0500

    try to zip

--- travis.sh
+++ travis.sh
@@ -22,6 +22,8 @@ if [[ "$TASK" == "clang" ]]; then
 	mkdir release && cd release && cmake -G Ninja .. -DCMAKE_BUILD_TYPE=Release && time ninja crossuo_unity -j8
 	file ./src/crossuo
 	file ./src/crossuo.so
+	echo Zip File
+	zip  CrossUO-clang-night.zip /release/src/crossuo.so
 	##./src/crossuo --headless
 	cd ..
 
